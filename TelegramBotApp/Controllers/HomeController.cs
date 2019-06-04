67115533289583ac49a85ccbd65bc980841250cf
Руслан <agishevrus@gmail.com>
2019-06-04 14:02:06 +0300

    Added empty start page and bootstrap package

---
+++ TelegramBotApp/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TelegramBotApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
