12287cdeb10e228a1c6ee955d55939c1c81c2090
Руслан <agishevrus@gmail.com>
2019-06-04 12:49:29 +0300

    Added migration

---
+++ TelegramBotApp/Migrations/20190604094830_Initial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TelegramBotApp.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BotButtons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Row = table.Column<int>(nullable: false),
+                    Column = table.Column<int>(nullable: false),
+                    ParentId = table.Column<int>(nullable: false),
+                    ButtonName = table.Column<string>(nullable: true),
+                    Content = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BotButtons", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BotButtons");
+        }
+    }
+}
